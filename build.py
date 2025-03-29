047f87dc5ca7f757c510c010dd745e7026ceb260
weizhonghua <wzh2012@outlook.com>
2025-03-30 00:03:58 +0800

    feat: 更新vuepress版本到v2

--- build.py
+++ build.py
@@ -23,11 +23,12 @@ class NavItem:
 @dataclass
 class Config:
     max_depth: int = 10
+    collapsible_depth: int = 2
     wiki: list[WikiItem] = field(default_factory=list)
     nav: list[NavItem] = field(default_factory=list)
 
 class WikiGenerator:
-    def __init__(self, root_dir: str, max_depth=10, nav: list[NavItem] = None):
+    def __init__(self, root_dir: str, max_depth=10, collapsible_depth=2, nav: list[NavItem] = None):
         self.root_dir = root_dir
         # self.wiki_dir = os.path.join(root_dir, ".wiki")
         self.vuepress_dir = os.path.join(root_dir, ".vuepress")
@@ -35,6 +36,7 @@ class WikiGenerator:
 
         self.roadmap_lists = []
         self.max_depth = max_depth
+        self.collapsible_depth = collapsible_depth
         self.nav = [] if nav is None else nav
 
     def get_realtive_path_of_wiki(self, wiki_name, path: dict):
@@ -44,28 +46,22 @@ class WikiGenerator:
     def _generate_roadmap_siderbar(self, wiki_name: str, data: dict, depth: int):
         res = []
         item_m = {}
+        collapsible = depth >= self.collapsible_depth
         for item in data["md_files"]:
-            key = item["path"][:-3] + ".dir"
-            p = self.get_realtive_path_of_wiki(wiki_name, item["path"])
+            # key = item["path"][:-3]
+            # p = self.get_realtive_path_of_wiki(wiki_name, item["path"])
             if item["is_leaf"]:
-                res.append([p, item["name"]])
-            else:
-                item_m.setdefault(key, {
-                    "title": item["name"],
-                    "collapsable": False,
-                    "children": [],
-                })["children"].append(
-                    [p, item["name"]]
-                )
+                res.append(item["name"])
         for item in data["subdirs"]:
             children = item_m.setdefault(item["key"], {
-                "title": item["name"],
-                "collapsable": False,
-                "children": [],
+                "text": item["name"],
+                "collapsible": collapsible,
+                "prefix": item["name"] + "/",
+                "children": [""],
             })["children"]
             if depth >= self.max_depth:
                 children.extend([
-                    [self.get_realtive_path_of_wiki(wiki_name, item["path"]), item["name"]]
+                    item["name"]
                     for item in item["md_files"]
                 ])
             else:
@@ -99,14 +95,14 @@ class WikiGenerator:
     """
             import_str += f"""import {item["key"]}Siderbar from "./sidebars/{item["key"]}Siderbar";\n"""
         data = f"""
-import {{SidebarConfig4Multiple}} from "vuepress/config";
+import {{ SidebarOptions }} from "@vuepress/theme-default";
+
 {import_str}
-// @ts-ignore
 export default {{
 {items_str}
     // 降级，默认根据文章标题渲染侧边栏
-    "/": "auto",
-}} as SidebarConfig4Multiple;
+    '/': "heading" ,
+}} as SidebarOptions;
     """
         file_path = os.path.join(self.vuepress_dir, "sidebar.ts")
         with open(file_path, 'w', encoding='utf-8') as f:
@@ -131,7 +127,7 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
         下载wiki
         """
         shutil.rmtree(self.get_wiki_dir(wiki_name), ignore_errors=True)
-        os.system(f"""feishu2md dl --wiki -o output_directory "{wiki_url}" """)
+        os.system(f"""./deploy/feishu2md dl --wiki -o output_directory "{wiki_url}" """)
 
     def generate_wiki(self, wiki_name: str, wiki_url: str):
         """
@@ -140,37 +136,45 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
         wiki_dir = self.get_wiki_dir(wiki_name)
 
         self.download_wiki(wiki_name, wiki_url)
+        self.move_static_directory(wiki_name)
 
-        self.change_directory_name(wiki_dir)
+        # self.change_directory_name(wiki_dir)
 
         data = self.generate_directory_listing(wiki_name, wiki_dir, 0)
         print(data)
         self.generate_roadmap_siderbar(wiki_name, data)
 
-        self.move_static_directory(wiki_name)
 
 
     def move_static_directory(self, wiki_name: str):
         """
-        遍历所有子目录的static目录，将其按原本的结构拷贝到.vuepress/public下
+        遍历所有子目录的 static 目录，将其按原本的结构拷贝到 .vuepress/public 下，
+        注意从最深层开始处理，避免因目录嵌套导致覆盖问题
         """
+        # 目标根目录：.vuepress/public/wiki/<wiki_name>
         static_base_dir = os.path.join(self.static_base_dir, wiki_name)
         os.makedirs(static_base_dir, exist_ok=True)
-        target_static_dir = os.path.join(static_base_dir, "static")
-        source_static_dir = os.path.join(self.get_wiki_dir(wiki_name), "static")
-        if os.path.exists(source_static_dir):
-            if os.path.exists(target_static_dir):
-                # 如果static目录已经存在，先删除
-                shutil.rmtree(target_static_dir)
-            os.rename(source_static_dir, target_static_dir)
+        source_wiki_dir = self.get_wiki_dir(wiki_name)
+
+        # 从最深处开始遍历
+        for root, dirs, files in os.walk(source_wiki_dir, topdown=False):
+            if "static" in dirs:
+                source_static_dir = os.path.join(root, "static")
+                # 计算 source_static_dir 相对于 source_wiki_dir 的路径，
+                # 这样可以在目标目录中还原原有的层级结构
+                rel_path = os.path.relpath(root, start=source_wiki_dir)
+                target_static_dir = os.path.join(static_base_dir, rel_path, "static")
+                os.makedirs(os.path.dirname(target_static_dir), exist_ok=True)
+                if os.path.exists(target_static_dir):
+                    # 如果目标目录存在，先删除
+                    shutil.rmtree(target_static_dir)
+                os.rename(source_static_dir, target_static_dir)
 
     def get_realname_dir_name(self: str, directory: str):
         """
-        Get the name of the directory, excluding the ".dir" suffix.
+        Get the name of the directory
         """
         directory_name = os.path.basename(directory)
-        if directory_name.endswith(".dir"):
-            directory_name = directory_name[:-4]
         return directory_name
 
     def change_directory_name(self, directory: str):
@@ -207,11 +211,28 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
         """
         dir_name = self.get_realname_dir_name(directory)
         try:
-            # Skip hidden directories and static directory
-            if os.path.basename(directory).startswith('.') or '/static/' in directory:
+            # Skip hidden directories
+            if os.path.basename(directory).startswith('.'):
                 return
 
             print(f"Processing directory: {directory}")
+
+            subdirs = []
+
+            # 如果某个文档有子目录，将这个文档移动到子目录中，并命名为README.md
+            # Get directory content
+            try:
+                dir_content = os.listdir(directory)
+            except Exception as e:
+                print(f"  Error listing directory contents: {e}")
+                return
+            for item in dir_content:
+                item_path = os.path.join(directory, item)
+                if os.path.isdir(item_path) and not item.startswith('.'):
+                    if os.path.exists(item_path + ".md"):
+                        os.rename(item_path + ".md", os.path.join(item_path, "README.md"))
+                    subdirs.append(item)
+
             readme_path = os.path.join(directory, 'README.md')
 
             # Read existing README content if it exists
@@ -237,21 +258,9 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
                 existing_content = f"# {dir_name}\n\n"
                 print(f"  No README.md found, will create one with title: {dir_name}")
 
-            # Get directory content
-            try:
-                dir_content = os.listdir(directory)
-            except Exception as e:
-                print(f"  Error listing directory contents: {e}")
-                return
-
             # Start building directory listing
-            content_listing = f"## 概述\n\n"
+            content_listing = f""
             # List subdirectories
-            subdirs = []
-            for item in dir_content:
-                item_path = os.path.join(directory, item)
-                if os.path.isdir(item_path) and not item.startswith('.') and item != 'static':
-                    subdirs.append(item)
 
             # if subdirs:
             #     content_listing += "### 子目录\n\n"
@@ -267,7 +276,7 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
                     md_files.append(item)
 
             if md_files:
-                content_listing += "### 文档\n\n"
+                content_listing += "## 子文档\n\n"
                 for md_file in sorted(md_files):
                     title = os.path.splitext(md_file)[0]
                     content_listing += f"- [{title}](./{md_file})\n"
@@ -290,8 +299,8 @@ export default {json.dumps(self.nav, indent=4, ensure_ascii=False)} as NavItem[]
             res = {
                 "subdirs": [],
                 "key": directory,
-                "name": os.path.basename(directory)[:-4],
-                "md_files": [{"path": os.path.join(directory, md_file), "name": md_file[:-3], "is_leaf": md_file[:-3] + ".dir" not in subdirs } for md_file in md_files],
+                "name": os.path.basename(directory),
+                "md_files": [{"path": os.path.join(directory, md_file), "name": md_file[:-3], "is_leaf": md_file[:-3] not in subdirs } for md_file in md_files],
             }
             # Process subdirectories recursively
             for subdir in subdirs:
@@ -317,9 +326,12 @@ if __name__ == "__main__":
     else:
         root_dir = "."
     config = read_config()
-    generator = WikiGenerator(root_dir, max_depth=config.max_depth, nav=config.nav)
-    # for item in config.wiki:
-    #     wiki_name, wiki_url = item["name"], item["url"]
-    #     generator.generate_wiki(wiki_name, wiki_url)
+    generator = WikiGenerator(root_dir,
+        max_depth=config.max_depth,
+        collapsible_depth=config.collapsible_depth,
+        nav=config.nav)
+    for item in config.wiki:
+        wiki_name, wiki_url = item["name"], item["url"]
+        generator.generate_wiki(wiki_name, wiki_url)
     generator.generate_sidebar()
     generator.generate_navbar()
